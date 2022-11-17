9e0af407b856345d8be9b7ff92f51ca959361eb6
Tyler Inari <github@al.tyler.jp>
2022-11-17 14:44:02 -0800

    add some params and export class

--- src/classes/userClass.js
+++ src/classes/userClass.js
@@ -1,21 +1,35 @@
-class userClass{
+class userClass {
   uId;
   fName;
   lName;
   email;
+  password;
   address;
   gender;
   age;
   country;
 
-  constructor(uId, fName, lName, email, address, gender, age, country){
+  constructor(
+    uId,
+    fName,
+    lName,
+    email,
+    password,
+    address,
+    gender,
+    age,
+    country
+  ) {
     this.uId = uId;
     this.fName = fName;
     this.lName = lName;
     this.email = email;
+    this.password = password;
     this.address = address;
     this.gender = gender;
     this.age = age;
     this.country = country;
   }
 }
+
+export default userClass;
