3d57f4cfd8da381c9d70229f4c50cc3ce58fb1f0
Tyler Inari <github@al.tyler.jp>
2022-11-17 14:44:02 -0800

    add some params and export class

--- src/classes/productClass.js
+++ src/classes/productClass.js
@@ -1,4 +1,4 @@
-class productClass{
+class productClass {
   pId;
   pName;
   price;
@@ -14,7 +14,22 @@ class productClass{
   stdev;
   reviewers_num;
 
-  constructor(pId, pName, price, country, type, image_path01, image_path02, image_path03, pClass, super_cluster, cluster, meta_critic, stdev, reviewers_num){
+  constructor(
+    pId,
+    pName,
+    price,
+    country,
+    type,
+    image_path01,
+    image_path02,
+    image_path03,
+    pClass,
+    super_cluster,
+    cluster,
+    meta_critic,
+    stdev,
+    reviewers_num
+  ) {
     this.pId = pId;
     this.pName = pName;
     this.price = price;
@@ -23,6 +38,7 @@ class productClass{
     this.image_path01 = image_path01;
     this.image_path02 = image_path02;
     this.image_path03 = image_path03;
+    this.pClass = pClass;
     this.super_cluster = super_cluster;
     this.cluster = cluster;
     this.meta_critic = meta_critic;
@@ -30,3 +46,5 @@ class productClass{
     this.reviewers_num = reviewers_num;
   }
 }
+
+export default productClass;
