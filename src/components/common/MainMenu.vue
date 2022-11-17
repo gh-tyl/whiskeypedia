c2c74607f854c7c3e587cb7592af499650742d38
Tyler Inari <github@al.tyler.jp>
2022-11-17 15:42:03 -0800

    create componets for mainmenu

---
+++ src/components/common/MainMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/products">Products</router-link>
+      </li>
+      <li>
+        <router-link to="/cocktails">Cocktails</router-link>
+      </li>
+      <li>
+        <router-link to="/contact">Contact</router-link>
+      </li>
+      <li>
+        <router-link to="/profile">Profile</router-link>
+      </li>
+      <li>
+        <router-link to="/cart">Cart</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MainMenu",
+};
+</script>
