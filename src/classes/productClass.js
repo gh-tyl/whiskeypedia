22906a23ef47b9a310a28fa77a08febc06e036ff
Junbro0708 <aktm708@naver.com>
2022-11-17 00:22:10 -0800

    Created readJson classes, basic user and product classes

---
+++ src/classes/productClass.js
@@ -0,0 +1,32 @@
+class productClass{
+  pId;
+  pName;
+  price;
+  country;
+  type;
+  image_path01;
+  image_path02;
+  image_path03;
+  pClass;
+  super_cluster;
+  cluster;
+  meta_critic;
+  stdev;
+  reviewers_num;
+
+  constructor(pId, pName, price, country, type, image_path01, image_path02, image_path03, pClass, super_cluster, cluster, meta_critic, stdev, reviewers_num){
+    this.pId = pId;
+    this.pName = pName;
+    this.price = price;
+    this.country = country;
+    this.type = type;
+    this.image_path01 = image_path01;
+    this.image_path02 = image_path02;
+    this.image_path03 = image_path03;
+    this.super_cluster = super_cluster;
+    this.cluster = cluster;
+    this.meta_critic = meta_critic;
+    this.stdev = stdev;
+    this.reviewers_num = reviewers_num;
+  }
+}
