89dd3ea78fa061832febf6d74a94a3abf0091497
Junbro0708 <aktm708@naver.com>
2022-11-17 00:22:10 -0800

    Created readJson classes, basic user and product classes

---
+++ src/classes/userClass.js
@@ -0,0 +1,21 @@
+class userClass{
+  uId;
+  fName;
+  lName;
+  email;
+  address;
+  gender;
+  age;
+  country;
+
+  constructor(uId, fName, lName, email, address, gender, age, country){
+    this.uId = uId;
+    this.fName = fName;
+    this.lName = lName;
+    this.email = email;
+    this.address = address;
+    this.gender = gender;
+    this.age = age;
+    this.country = country;
+  }
+}
