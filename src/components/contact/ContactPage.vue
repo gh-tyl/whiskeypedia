cd7f08ba68632fd778ba20d5569749a25e2fd827
ka-cutler <kacutlerwd@gmail.com>
2022-11-17 18:41:31 -0800

    contact html done

--- src/components/contact/ContactPage.vue
+++ src/components/contact/ContactPage.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="contact-page">
-    <h1>Contact</h1>
+  <div>
+    <aside></aside>
   </div>
+  <div class="contact-page">
+    <h1><aside>CONTACT US</aside></h1>
+    <p>We would love to hear from you! Get in touch and a member of our team will get back to you as soon as possible.</p>
+    <div class="contact-main">
+      <aside></aside>
+      <form>
+        <span>*indicates required field</span>
+        <input type="text" placeholder="First Name*" required/>
+        <input type="text" placeholder="Last Name*" required/>
+        <input class="email" type="email" placeholder="Email*" required/>
+        <input type="text" placeholder="Order Number"/>
+        <input type="text" placeholder="Subject"/>
+        <textarea placeholder="Message*" required />
+        <button type="button">SEND</button>
+      </form>
+      <aside class="footer-logo"></aside>
+      <p>Please enjoy responsibly.</p>
+    </div>
+  </div>
+  <div><span> &copy; Copyright 2022 </span></div>
 </template>
 
 <script>
@@ -9,3 +29,5 @@ export default {
   name: "ContactPage",
 };
 </script>
+
+
