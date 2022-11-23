6f03d79d1b573fcbe1588183676c9f17147d56c5
Tyler Inari <github@al.tyler.jp>
2022-11-23 13:14:58 -0800

    done map and show products without css

--- src/components/products/ProductsPage.vue
+++ src/components/products/ProductsPage.vue
@@ -2,28 +2,45 @@
   <div class="products-page">
     <h1>Products</h1>
     <div>
-      <ProductsMapCompo :products="products" />
+      <ProductsMapCompo :products="products" @country="getCountry" />
+      <ProductsListCompo :products="displayProds" />
     </div>
   </div>
 </template>
 
 <script>
 import ProductsMapCompo from "./MapCompo.vue";
+import ProductsListCompo from "./ListCompo.vue";
+
 export default {
   name: "ProductsPage",
   components: {
     ProductsMapCompo,
+    ProductsListCompo,
   },
   props: {
     products: Array,
   },
   data() {
     return {
+      displayProds: new Array(),
+      country: "",
     };
   },
   methods: {
+    getCountry(country) {
+      this.country = country;
+      console.log(this.country);
+      this.filterCountry();
+    },
+    filterCountry() {
+      this.displayProds = this.products.filter((product) => {
+        return product.country == this.country;
+      });
+    },
   },
   mounted() {
+    this.displayProds = this.products;
   },
 };
 </script>
